862f2ee73caa0baa99207508602ff210c5f84ca0
lukeboi <lukefarritor@gmail.com>
2023-05-05 23:06:09 -0500

    Update full_scroll_downloader.py

--- full_scroll_downloader.py
+++ full_scroll_downloader.py
@@ -3,6 +3,8 @@ import requests
 from requests.auth import HTTPBasicAuth
 from PIL import Image
 import numpy as np
+import argparse
+from tqdm import tqdm
 from config import url_template, username, password
 
 output_folder = "D:\small_scroll_one"
@@ -11,9 +13,7 @@ if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 def download_image(url, filename):
-    print(url)
     response = requests.get(url, auth=HTTPBasicAuth(username, password))
-    print(response.status_code)
 
     if response.status_code == 200:
         with open(filename, "wb") as f:
@@ -37,11 +37,27 @@ def resize_image(input_filename, output_filename, scale_factor):
         resized_img = img.resize(new_size, Image.ANTIALIAS)
         resized_img.save(output_filename, format="JPEG")
 
-for i in range(0, 7250, 8):
-    original_filename = os.path.join(output_folder, f"0{i:04}.tif")
-    resized_filename = os.path.join(output_folder, f"{i:04}_resized.jpg")
-    url = url_template.format(i)
-    
-    if download_image(url, original_filename):
-        resize_image(original_filename, resized_filename, 8)
-        os.remove(original_filename)
+def main(start, end, step):
+    for i in tqdm(range(start, end, step)):
+        original_filename = os.path.join(output_folder, f"0{i:04}.tif")
+        resized_filename = os.path.join(output_folder, f"{i:04}_resized.jpg")
+        url = url_template.format(i)
+
+        # Check if output image already exists, and if so, skip download and resize
+        if os.path.exists(resized_filename):
+            print(f"Output image {resized_filename} already exists. Skipping download and resize.")
+            continue
+
+        if download_image(url, original_filename):
+            resize_image(original_filename, resized_filename, 8)
+            os.remove(original_filename)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Download and resize images.')
+    parser.add_argument('start', type=int, help='Start index')
+    parser.add_argument('end', type=int, help='End index (exclusive)')
+    parser.add_argument('step', type=int, help='Step size')
+    args = parser.parse_args()
+
+    main(args.start, args.end, args.step)

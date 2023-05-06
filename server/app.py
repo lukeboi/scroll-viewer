ade15763ec8710dd13ba364cb56d86c819030522
lukeboi <lukefarritor@gmail.com>
2023-05-06 11:41:50 -0500

    Begin work on heartbeat

--- server/app.py
+++ server/app.py
@@ -16,6 +16,9 @@ CORS(app)
 json_file = "config.json"
 import traceback
 
+# global status message for the user
+server_status = "yoinky sploinky"
+
 @app.errorhandler(Exception)
 def handle_exception(e):
     #Use for stack trace
@@ -71,6 +74,15 @@ def get_volume_from_tif_stack(src, origin, size):
         print(i)
 
     return raw_data
+
+
+@app.route('/volume_metadata', methods=['GET'])
+def get_volume_metadata():
+    return send_file("config.json")
+
+@app.route('/heartbeat', methods=['GET'])
+def get_heartbeat():
+    return server_status
 
 @app.route('/volume', methods=['GET'])
 def volume():
@@ -157,44 +169,6 @@ def volume():
     except Exception as e:
         return f"Error: {str(e)}", 500
 
-
-@app.route('/load_subvolume', methods=['GET'])
-def load_subvolume():
-    try:
-        json_file = request.args.get('json_file')
-        volume_filename = request.args.get('volume_filename')
-
-        with open(json_file, 'r') as f:
-            config = json.load(f)
-
-        if volume_filename is None:
-            volume_filename = config.get('volume_filename')
-
-        if not os.path.exists(volume_filename):
-            return f"Error: Volume file not found at '{volume_filename}'", 404
-
-        subvolume_coords = config.get('subvolume_coords')
-        if subvolume_coords is None:
-            return "Error: 'subvolume_coords' not found in the JSON configuration.", 400
-
-        x1, y1, z1, x2, y2, z2 = subvolume_coords
-
-        volume_data = np.fromfile(volume_filename, dtype=np.uint8)
-        volume_shape = config.get('volume_shape', (256, 256, 256))  # Assuming a default shape
-        volume = np.reshape(volume_data, volume_shape)
-
-        subvolume = volume[x1:x2, y1:y2, z1:z2]
-
-        # Create a binary stream to store the subvolume data
-        binary_stream = BytesIO()
-        binary_stream.write(subvolume.tobytes())
-        binary_stream.seek(0)
-
-        # Serve the .raw file as a static file
-        return send_file(binary_stream, download_name="subvolume.raw", as_attachment=True)
-
-    except Exception as e:
-        return f"Error: {str(e)}", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
